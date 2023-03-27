eb65a7efcea3498529ce6c6dd8c384223d694616
vaibhavt896 <104461979+vaibhavt896@users.noreply.github.com>
2023-03-27 23:47:59 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,125 @@
+export const tweetsData = [   
+    {
+        handle: `@TrollBot66756542 💎`,
+        profilePic: `images/flower.png`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
+            Guaranteed return on investment. HMU DMs open!!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Elon ✅`,
+        profilePic: `images/elon.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
+        replies: [
+                  {
+                handle: `@TomCruise ✅`,
+                profilePic: `images/tcruise.png`,
+                tweetText: `Yes! Sign me up! 😎🛩`,
+            },
+                  {
+                handle: `@ChuckNorris ✅`,
+                profilePic: `images/chucknorris.jpeg`,
+                tweetText: `I went last year😴`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@NoobCoder12`,
+        profilePic: `images/noob.jpeg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Are you a coder if you only know HTML?`,
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
+            },
+            {
+                handle: `@YummyCoder64`,
+                profilePic: `images/love.png`,
+                tweetText: `You are wonderful just as you are! ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    }, 
+    {
+        handle: '@FoodieLife 🍔',
+        profilePic: 'images/food.png',
+        likes: 32,
+        retweets: 12,
+        tweetText: 'Just had the best burger of my life at this new place in town! You have to try it out! #foodie #yum',
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '2f2d0a4e-7c8d-4f9c-bf1a-1d7b8e3d0f3e'
+    },
+    {
+        handle: '@TravelBug 🌎',
+        profilePic: 'images/travel.jpeg',
+        likes: 45,
+        retweets: 20,
+        tweetText: 'I just got back from the most amazing trip to Bali! Check out my blog for all the details and photos! #travel #wanderlust',
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'a2b9c8d1-6e7f-4b9a-a9c0-2d3f4e5b6c7d'
+    },
+    {
+        handle: '@FitnessFreak 🏋️‍♀️',
+        profilePic: 'images/fitness.jpeg',
+        likes: 18,
+        retweets: 5,
+        tweetText: 'Just finished an intense workout at the gym! Feeling great and ready to take on the day! 💪🏼 #fitness #motivation',
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'e8b0a7c3-3a9e-4f5e-b1d8-f2a1c0f6a2b0'
+    },
+    {
+        handle: '@TechieTalk 🖥️',
+        profilePic: 'images/tech.jpeg',
+        likes: 23,
+        retweets: 8,
+        tweetText:
+            "Just got my hands on the new MacBook Pro and it's amazing! The display is stunning and the performance is lightning fast! #tech #apple",
+            replies:[],
+            isLiked:false,
+            isRetweeted:false,
+            uuid:'3d7b8e4c-a9b0-4f1a-bc2d-1e6f5d2c3d4e'
+    },
+    {
+      handle:'@MusicLover 🎶',
+      profilePic:'images/music-lover.jpeg',
+      likes:'20',
+      retweets:'5',
+      tweetText:'Just discovered this amazing new artist! Check out their latest album, it\'s 🔥🔥🔥 #music #newmusicfriday ',
+      replies:[],
+      isLiked:false,
+      isRetweeted:false,
+      uuid:'1a2b3c4d-e5f6-g7h8-i9j0-k1l2m3n4o5p'
+    },
+    {
+        handle: '@CryptoKing123 🚀',
+        profilePic: 'images/crypto-king.jpeg',
+        likes: 15,
+        retweets: 7,
+        tweetText: 'The future of money is here! Invest in crypto today and watch your wealth grow! #Bitcoin #Ethereum #Dogecoin',
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'c1d6a8e6-7a8e-4f9d-bc3d-2b1a6f5e6b5c'
+    }    
+]
---
+++ index.js
@@ -0,0 +1,238 @@
+import { tweetsData } from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+// Load tweetsData from local storage, if available
+const storedTweetsData = localStorage.getItem('tweetsData');
+if (storedTweetsData) {
+  tweetsData = JSON.parse(storedTweetsData);
+  render(); // call render function after retrieving data from local storage
+
+}
+
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+    else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
+    }
+    else if(e.target.id === 'tweet-btn'){
+        handleTweetBtnClick()
+    }
+    else if(e.target.dataset.delete){
+    handleDeleteClick(e.target.dataset.delete)
+}
+
+})
+function handleDeleteClick(tweetId){
+    const tweetIndex = tweetsData.findIndex(function(tweet){
+        return tweet.uuid === tweetId
+    })
+    
+    if(tweetIndex !== -1){
+        tweetsData.splice(tweetIndex, 1)
+                localStorage.setItem('tweetsData', JSON.stringify(tweetsData))
+
+        render()
+    }
+}
+
+ 
+function handleLikeClick(tweetId){ 
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }
+    else{
+        targetTweetObj.likes++ 
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+        localStorage.setItem('tweetsData', JSON.stringify(tweetsData))
+
+    render()
+}
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+        localStorage.setItem('tweetsData', JSON.stringify(tweetsData))
+
+    render() 
+}
+
+function handleReplyClick(replyId){
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+    const repliesDiv = document.getElementById(`replies-${replyId}`)
+    const replyInput = repliesDiv.querySelector('.reply-input')
+    const replyBtn = repliesDiv.querySelector('.reply-btn')
+
+    // Show/hide reply input field and button
+    replyInput.classList.toggle('hidden')
+    replyBtn.classList.toggle('hidden')
+
+    // Add event listener to submit button
+    replyBtn.addEventListener('click', function(){
+        const tweet = tweetsData.filter(function(tweet){
+            return tweet.uuid === replyId
+        })[0]
+// Create new object for reply
+        const newReply = {
+           handle: `@Vaibhav`,
+            profilePic: `images/vaibhav.png`,
+            likes: 0,
+            retweets: 0,
+            tweetText: replyInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4()
+        }
+
+        // Add new reply object to replies array of tweet
+        tweet.replies.unshift(newReply)
+
+        // Hide input field and button
+        replyInput.classList.toggle('hidden')
+        replyBtn.classList.toggle('hidden')
+ // Clear input field
+        replyInput.value = ''
+    localStorage.setItem('tweetsData', JSON.stringify(tweetsData))
+
+        // Update feed with new reply
+        render()
+    })
+}
+
+function handleTweetBtnClick(){
+    const tweetInput = document.getElementById('tweet-input')
+
+    if(tweetInput.value){
+        tweetsData.unshift({
+            handle: `@Vaibhav`,
+            profilePic: `images/vaibhav.png`,
+            likes: 0,
+            retweets: 0,
+            tweetText: tweetInput.value,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4()
+        })
+    render()
+    tweetInput.value = ''
+    }
+
+}
+
+function getFeedHtml(){
+    let feedHtml = ``
+    
+    tweetsData.forEach(function(tweet){
+        
+        let likeIconClass = ''
+        
+        if (tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+        
+        let retweetIconClass = ''
+        
+        if (tweet.isRetweeted){
+            retweetIconClass = 'retweeted'
+        }
+        
+        let repliesHtml = ''
+        
+        if(tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml+=`
+<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="${reply.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+            </div>
+        </div>
+</div>
+`
+            })
+        }
+        
+          
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+            
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart ${likeIconClass}"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+                <span class="tweet-detail">
+    <i class="fa-solid fa-trash" data-delete="${tweet.uuid}"></i>
+ 
+</span>
+            </div> 
+            <div class="hidden" id="replies-${tweet.uuid}">
+    ${repliesHtml}
+    <div class="reply-container">
+        <input type="text" class="reply-input hidden" placeholder="Reply...">
+        <button id="reply"class="reply-btn hidden">Reply</button>
+    </div>
+</div>  
+        </div>            
+    </div>
+    <div class="hidden" id="replies-${tweet.uuid}">
+        ${repliesHtml}
+    </div>
+    
+   
+</div>
+`
+   })
+   return feedHtml 
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
+
+localStorage.setItem('tweetsData', JSON.stringify(tweetsData));
+
